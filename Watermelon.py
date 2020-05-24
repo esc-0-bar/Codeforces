f6b64578ccd974ba7d465a0c8bc7582fe3640288
Mohammad Shiblee Noman Ahad <hello@iamahad.com>
2020-05-25 00:09:00 +0600

    solved problem/4/A

---
+++ Watermelon.py
@@ -0,0 +1,12 @@
+"""
+Author: Mohammad Shiblee Noman Ahad
+Email: hello@iamahad.com
+Website: www.iamahad.com
+Github: www.github.com/i-am-ahad
+"""
+
+
+w = int(input())
+if w % 2 == 0 and w > 2:
+    print("YES")
+else: print("NO")
