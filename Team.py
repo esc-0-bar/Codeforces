a917d1595f13fac01c1b1c4a3abceb2ef8746e7e
Mohammad Shiblee Noman Ahad <hello@iamahad.com>
2020-05-29 20:55:03 +0600

    solved problem/231/A

---
+++ Team.py
@@ -0,0 +1,14 @@
+"""
+Author: Mohammad Shiblee Noman Ahad
+Email: hello@iamahad.com
+Website: www.iamahad.com
+Github: www.github.com/i-am-ahad
+"""
+
+n = int(input())
+problems = []
+
+for x in range(n):
+    problems.append(input().split())
+    
+print(problems)
