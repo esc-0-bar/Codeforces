adb96cff33c548d182bb7c5db184d3f048a573d8
Mohammad Shiblee Noman Ahad <hello@iamahad.com>
2020-05-29 20:56:30 +0600

    solved problem/231/A(updated)

--- Team.py
+++ Team.py
@@ -7,8 +7,14 @@ Github: www.github.com/i-am-ahad
 
 n = int(input())
 problems = []
-
+submit = 0
 for x in range(n):
+    temp = 0
     problems.append(input().split())
-    
-print(problems)
+    for y in range(3):
+        problems[x][y] = int(problems[x][y])
+        temp = temp + problems[x][y]
+        if temp == 2 and problems[x][y]!=0:
+            submit += 1
+
+print(submit)
